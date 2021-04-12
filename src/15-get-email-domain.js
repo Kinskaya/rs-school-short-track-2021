13ab23f987117469b134d18ee8d2216c61e56a95
Kinskaya <kinskaya.nastya@gmail.com>
2021-04-12 22:50:50 +0300

    initial setup and tasks

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,19 +8,8 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-// eslint-disable-next-line consistent-return
-function getEmailDomain(email) {
-  const arr = email.split('');
-  const c = '@';
-  const domain = [];
-  // eslint-disable-next-line for-direction
-  for (let i = arr.length - 1; i < arr.length; i--) {
-    if (arr[i] !== c) {
-      domain.unshift(arr[i]);
-    } else if (arr[i] === c) {
-      return domain.join('');
-    }
-  }
+function getEmailDomain(/* email */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = getEmailDomain;
