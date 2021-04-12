af4f3d4611c7bd9941af385310f62cad7a7576ec
Kinskaya <kinskaya.nastya@gmail.com>
2021-04-12 23:41:28 +0300

    feat: add code 10-find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,19 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+// eslint-disable-next-line consistent-return
+function findIndex(array, value) {
+  let start = 0;
+  let end = array.length;
+  let middle = Math.floor((start + end) / 2);
+  for (let i = 0; i < array.length; i++) {
+    if (array[middle] !== value) {
+      if (value < array[middle]) end = middle;
+      else start = middle;
+      middle = Math.floor((start + end) / 2);
+    }
+    if (array[middle] === value) return middle;
+  }
 }
 
 module.exports = findIndex;
